2c03287bbe168928b8dad49ad621f8fd66d927b2
José Lisboa <josemrlisboa@gmail.com>
2016-09-14 16:59:37 -0300

    Request Controller updated.

--- aula4/request_controller.php
+++ aula4/request_controller.php
@@ -5,7 +5,8 @@ class RequestController
 {
 	const VALID_METHODS = array('GET', 'POST', 'PUT', 'DELETE');
 	const VALID_PROTOCOLS = array('HTTP/1.0', 'HTTPS/1.0', 'HTTP/1.1', 'HTTPS/1.1');
-
+	const VALID_PATHS = array('http://localhost/aula4/','http://localhost/aula4/index.php','http://localhost/aula4/request.php','http://localhost/aula4/request_controller.php');
+	
 	public function create_request($request_info)
 	{
 		if(!self::is_valid_method($request_info['REQUEST_METHOD']))
@@ -13,15 +14,18 @@ class RequestController
 			return array("code" => "405", "message" => "Method not allowed");
 			
 		}
-		if(!self::is_valid_protocol($request_info['SERVER_PROTOCOL'], 0, -4))
+		if(!self::is_valid_protocol($request_info['SERVER_PROTOCOL']))
 		{
-			return array("code" => "none", "message" => "Invalid protocol");
+			return array("code" => "400", "message" => "Invalid protocol");
 		}
-		if(self::is_valid_ip($request_info['REMOTE_ADDR']))
+		if(!self::is_valid_ip($request_info['REMOTE_ADDR']))
 		{
-			return array("code" => "OK", "message" => "IP valido");
+			return array("code" => "400", "message" => "Invalid ip address");
 		}
-			
+		if(!self::is_valid_path($request_info['HTTP_HOST'].$request_info['PHP_SELF']))
+		{
+			return array("code" => "400", "message" => "Invalid path");
+		}
 		return true;
 
 
@@ -55,10 +59,18 @@ class RequestController
 	
 	public function is_valid_ip($ip)
 	{
-		if (filter_var($ip, FILTER_VALIDATE_IP))
-			return true;
+		if (!filter_var($ip, FILTER_VALIDATE_IP))
+			return false;
 
-		return false;
+		return true;
+	}
+	
+	public function is_valid_path($url)
+	{
+		if (is_null($url) || !in_array($url, self::VALID_PATHS))
+			return false;
+		
+		return true;
 	}
 
 

7850fa6452321ff195cf293a46b593407911bbed
root <miletotg@gmail.com>
2016-10-25 20:49:47 -0200

    Aula8

---
+++ client_aula8/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<body>
+ 
+<form action="requests.php" method="post">
+  <input type="text" name="first_name" value="firstname"><br>
+<input type="text" name="last_name" value="lastname"><br>
+<input type="text" name="email" value="email"><br>
+<input type="text" name="password" value="senha"><br>
+<input type="text" name="gender" value="Genero"><br>
+<input type="text" name="birthdate" value="Nasc"><br>
+  <input type="submit" value="Submit">
+
+
+</form> 
+
+</body>
+
+
+
+</html>
+
+
+
